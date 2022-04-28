e927d27f8a77665091a75c013c7aaa7ddfceca07
RaphaelDaubelcour <r.daubelcour@student.isartdigital.com>
2022-04-28 15:35:58 -0400

    Better new Drag

--- Project Card Trader/Assets/2 script/Card script/Card.cs
+++ Project Card Trader/Assets/2 script/Card script/Card.cs
@@ -9,6 +9,7 @@ public class Card : MonoBehaviour
 {
     public int index;
     public bool isPlayed;
+    public bool beingDrag;
 
 	public GameManager gm;
 
@@ -29,11 +30,30 @@ public class Card : MonoBehaviour
 		}
 	}
 
-	void MoveToDiscardPile()
+    private void Update()
+    {
+        if (!beingDrag)
+        {
+			//Retourne dans la main
+			print("Retrun to hand");
+		}
+		beingDrag = false;
+	}
+
+    void MoveToDiscardPile()
 	{
 		gm.hand.Remove(this);
 		gm.discard.Add(this);
 		gameObject.SetActive(false);
+	}
+
+    private void OnMouseDrag()
+    {
+		beingDrag = true;
+		Vector3 position = new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.WorldToScreenPoint(transform.position).z);
+		Vector3 worldPosition = Camera.main.ScreenToWorldPoint(position);
+		transform.position = worldPosition;
+
 	}
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -41,6 +61,7 @@ public class Card : MonoBehaviour
         if(collision.CompareTag("Shop"))
         {
 			//Ici tu appelle l'effet de la carte bg
+			print("Play Card");
         }
     }
 }
